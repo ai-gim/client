79559cc0810e3bdac5424b1a1debf689ec96a3a9
鲁洋 <luyang@asiainfo.com>
2015-09-23 15:38:02 +0800

    1.alert查询api完成。

---
+++ src/main/java/com/asiainfo/gim/client/monitor/domain/Alert.java
@@ -0,0 +1,119 @@
+package com.asiainfo.gim.client.monitor.domain;
+
+import java.util.Date;
+import java.util.Map;
+
+public class Alert
+{
+	private String id;
+	private Date time;
+	private Integer targetType;
+	private String targetId;
+	private Integer level;
+	private Integer status;
+	private String source;
+	private Integer configId;
+	private String description;
+	private Map<String, String> properties;
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public Date getTime()
+	{
+		return time;
+	}
+
+	public void setTime(Date time)
+	{
+		this.time = time;
+	}
+
+	public String getTargetId()
+	{
+		return targetId;
+	}
+
+	public void setTargetId(String targetId)
+	{
+		this.targetId = targetId;
+	}
+
+	public String getSource()
+	{
+		return source;
+	}
+
+	public void setSource(String source)
+	{
+		this.source = source;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+	public Map<String, String> getProperties()
+	{
+		return properties;
+	}
+
+	public void setProperties(Map<String, String> properties)
+	{
+		this.properties = properties;
+	}
+
+	public Integer getTargetType()
+	{
+		return targetType;
+	}
+
+	public void setTargetType(Integer targetType)
+	{
+		this.targetType = targetType;
+	}
+
+	public Integer getLevel()
+	{
+		return level;
+	}
+
+	public void setLevel(Integer level)
+	{
+		this.level = level;
+	}
+
+	public Integer getStatus()
+	{
+		return status;
+	}
+
+	public void setStatus(Integer status)
+	{
+		this.status = status;
+	}
+
+	public Integer getConfigId()
+	{
+		return configId;
+	}
+
+	public void setConfigId(Integer configId)
+	{
+		this.configId = configId;
+	}
+
+}
