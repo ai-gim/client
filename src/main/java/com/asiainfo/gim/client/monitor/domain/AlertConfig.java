83bb935e0d78551bf4d1fff82c1f69d1b74975ad
wangjy <wangjy7@yourmaili.com>
2015-09-24 17:04:15 +0800

    1。告警配置功能接口

---
+++ src/main/java/com/asiainfo/gim/client/monitor/domain/AlertConfig.java
@@ -0,0 +1,75 @@
+package com.asiainfo.gim.client.monitor.domain;
+
+public class AlertConfig
+{
+	private Integer id;
+	private Integer targetType;
+	private String targetId;
+	// 告警级别
+	private Integer level;
+	// 告警类型(metric, heartbeat)
+	private Integer type;
+	//专属字段的json数据
+	private String properties;
+
+	public Integer getId()
+	{
+		return id;
+	}
+
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
+
+	public String getTargetId()
+	{
+		return targetId;
+	}
+
+	public void setTargetId(String targetId)
+	{
+		this.targetId = targetId;
+	}
+
+	public Integer getTargetType()
+	{
+		return targetType;
+	}
+
+	public void setTargetType(Integer targetType)
+	{
+		this.targetType = targetType;
+	}
+
+	public Integer getLevel()
+	{
+		return level;
+	}
+
+	public void setLevel(Integer level)
+	{
+		this.level = level;
+	}
+
+	public Integer getType()
+	{
+		return type;
+	}
+
+	public void setType(Integer type)
+	{
+		this.type = type;
+	}
+
+	public String getProperties()
+	{
+		return properties;
+	}
+
+	public void setProperties(String properties)
+	{
+		this.properties = properties;
+	}
+
+}
